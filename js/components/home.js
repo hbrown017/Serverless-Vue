203ee6d636e958642e4f25a4cc14d06dbcbea426
hbrown017 <silver_heart017@yahoo.com>
2020-02-17 17:52:11 -0600

    Add menu hover color

--- js/components/home.js
+++ js/components/home.js
@@ -7,7 +7,7 @@ const HomeTemplate = {
     </div>`,
     data(){
         return{
-            text: 'This is a front-end site structured using Vue JS routing and components for code reusability and cost efficiency; ideal for a serverless architecture approach.',
+            text: 'This is a front-end site structured using VueJS routing and components for code reusability and cost efficiency; ideal for a serverless architecture approach.',
         }
     }
 };
